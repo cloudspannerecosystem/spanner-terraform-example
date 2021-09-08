079eeb18e8cd73a7f9ded89e2df9aa1a883ad84c
sserban-us <sserban@google.com>
2021-09-08 16:53:13 -0700

    Updating google provider version to the latest
    This adds support for PUs.

--- examples/cloud-spanner/main.tf
+++ examples/cloud-spanner/main.tf
@@ -3,7 +3,7 @@ terraform {
 }
 
 provider "google" {
-  version = "3.51.0" # see https://github.com/terraform-providers/terraform-provider-google/releases
+  version = "3.82.0" # see https://github.com/terraform-providers/terraform-provider-google/releases
   project = var.project
 }
 
